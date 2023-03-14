7c31ed8fca7099a4e3c4d6996699c3bfb3e9eaf9
Dev-Muller <pedromullergl11@gmail.com>
2023-03-14 14:07:00 -0300

    feat: criado form com input, botao para pesqueisar artista e funcao para habilitar botao

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,38 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  state = {
+    disabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const MINNumber = 2;
+    const valor = target.value;
+    this.setState({ [target.name]: valor, disabled: valor.length < MINNumber });
+  };
+
   render() {
+    const { disabled } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <p>Hello World!</p>
+          <form action="">
+            <input
+              name="artistName"
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.onInputChange }
+              placeholder="Nome do Artista"
+            />
+            <button
+              disabled={ disabled }
+              type="submit"
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </>
     );
