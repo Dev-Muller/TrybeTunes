b70eacc1e330895a4b4440b37f160f5b92c7aa99
Dev-Muller <pedromullergl11@gmail.com>
2023-03-15 07:39:51 -0300

    feat: feito fetch da API de procurar albuns, fazendo a funcao dar o fetch e liberar o botao de pesquisar apos passar na verificao

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Cards extends React.Component {
+  render() {
+    return (
+      <ul>
+        <div>
+          <h1>{}</h1>
+          <h2>{}</h2>
+          <img src="" alt="" />
+        </div>
+      </ul>
+    );
+  }
+}
+
+Cards.propTypes = {
+
+};
+
+export default Cards;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import Carregando from '../Components/Carregando';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     disabled: true,
+    loading: false,
+    name: '',
+    nameInput: '',
+    albumList: [],
+    result: false,
   };
 
   onInputChange = ({ target }) => {
@@ -12,28 +20,62 @@ class Search extends React.Component {
     this.setState({ [target.name]: valor, disabled: valor.length < MINNumber });
   };
 
+  handlerBtn = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    this.setState({ loading: true, name });
+    const searchAPI = await searchAlbumsAPI(name);
+    this.setState({
+      loading: false,
+      nameInput: name,
+      name: '',
+      albumList: searchAPI,
+      result: true });
+  };
+
   render() {
-    const { disabled } = this.state;
+    const { disabled, loading, nameInput, name, albumList, result } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
+          { loading && <Carregando /> }
           <form action="">
             <input
-              name="artistName"
+              name="name"
               type="text"
+              value={ name }
               data-testid="search-artist-input"
               onChange={ this.onInputChange }
               placeholder="Nome do Artista"
             />
             <button
               disabled={ disabled }
-              type="submit"
+              type="button"
               data-testid="search-artist-button"
+              onClick={ this.handlerBtn }
             >
               Pesquisar
             </button>
           </form>
+          { result && <p>{ `Resultado de álbuns de: ${nameInput}` }</p>}
+          <ul>
+            { albumList
+              .length === 0 ? <li>Nenhum álbum foi encontrado</li>
+              : albumList.map((album) => (
+                <li
+                  key={ album.collectionId }
+                >
+                  <Link
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                    to={ `/album/${album.collectionId}` }
+                  >
+                    {album.collectionName}
+                  </Link>
+                </li>
+              )) }
+          </ul>
+
         </div>
       </>
     );
