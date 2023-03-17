d83d2eabd3024b2d82e8e93852908f269975bff0
Dev-Muller <pedromullergl11@gmail.com>
2023-03-17 07:48:31 -0300

    feat: adicionado informacoes do usuario para a pagina de profile e link de editar perfil para a pagina de editar perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import Carregando from '../Components/Carregando';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    loading: false,
+    userData: {},
+  };
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  getUserData = async () => {
+    this.setState({ loading: true });
+    const userData = await getUser();
+    this.setState({ loading: false, userData });
+  };
+
   render() {
+    // const { statsUser } = this.props;
+    const { loading, userData } = this.state;
     return (
       <div>
         <Header />
+        { loading && <Carregando />}
         <div data-testid="page-profile">
-          <p>Hello World!</p>
+          <div>
+            <img src={ userData.image } alt="" data-testid="profile-image" />
+            <Link to="/profile/edit">
+              Editar perfil
+            </Link>
+          </div>
+          <label htmlFor="name">
+            Nome:
+            <p id="name">{ userData.name }</p>
+          </label>
+          <label htmlFor="email">
+            E-mail:
+            <p id="email">{ userData.email }</p>
+          </label>
+          <label htmlFor="description">
+            Description:
+            <p id="description">{ userData.description }</p>
+          </label>
         </div>
       </div>
     );
